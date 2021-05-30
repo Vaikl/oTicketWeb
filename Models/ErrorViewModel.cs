1de5d46d6e9d93c3c4e6062bc88a64c60cc5032b
Иван Хвещук <vaikl2012@gmail.com>
2021-05-30 18:48:19 +0300

    ДИЗАЙН 2

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Clinic_master.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
